11790a475b84efe226cdc9e8ccee3c31377a653c
Cappecasper03 <03repsac@gmail.com>
2025-04-10 21:41:04 +0200

    Removed light mode switch

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.getElementById('theme-toggle');
-    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
     const navHeight = document.querySelector('nav').offsetHeight; // Get the height of the fixed navigation
-
-    // Check if dark mode is preferred
-    if (prefersDarkScheme.matches) {
-        document.documentElement.classList.add('dark');
-    }
-
-    // Add event listener for theme toggle
-    themeToggle.addEventListener('click', () => {
-        document.documentElement.classList.toggle('dark');
-    });
 
     // Add smooth scrolling for navigation links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
